bf6c9518f9a67473899b607fac8045655c045315
Truongyd05 <phungdangtruong77@gmail.com>
2025-09-30 11:02:14 +0700

    bonus getter/setter

--- CosmeticShop/src/main/java/Model/Product.java
+++ CosmeticShop/src/main/java/Model/Product.java
@@ -20,11 +20,61 @@ public class Product {
     }
 
     // Getter & Setter
-    public int getProductId() { return productId; }
-    public String getName() { return name; }
-    public double getPrice() { return price; }
-    public int getStock() { return stock; }
-    public String getDescription() { return description; }
-    public String getImageUrl() { return imageUrl; }
-    public int getCategoryId() { return categoryId; }
+
+    public int getProductId() {
+        return productId;
+    }
+
+    public void setProductId(int productId) {
+        this.productId = productId;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public double getPrice() {
+        return price;
+    }
+
+    public void setPrice(double price) {
+        this.price = price;
+    }
+
+    public int getStock() {
+        return stock;
+    }
+
+    public void setStock(int stock) {
+        this.stock = stock;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public String getImageUrl() {
+        return imageUrl;
+    }
+
+    public void setImageUrl(String imageUrl) {
+        this.imageUrl = imageUrl;
+    }
+
+    public int getCategoryId() {
+        return categoryId;
+    }
+
+    public void setCategoryId(int categoryId) {
+        this.categoryId = categoryId;
+    }
+    
 }
